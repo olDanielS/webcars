68e81acf438d6793781504b61366d191c0727591
Daniel Oliveira <danielnrt80@gmail.com>
2025-03-10 23:11:28 -0300

    feat: auth context

---
+++ src/contexts/auth.tsx
@@ -0,0 +1,55 @@
+import {ReactNode, useState, createContext, useEffect} from 'react';
+
+import {auth} from '../services/firebaseConnection'
+import { onAuthStateChanged} from 'firebase/auth';
+
+interface AuthProviderProps{
+  children: ReactNode;
+}
+
+type AuthContextData = {
+  signed: boolean;
+  loading: boolean;
+}
+
+type UserProps = {
+  uid: string,
+  name?: string | null,
+  email?: string | null
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+function AuthProvider({children}: AuthProviderProps) {
+  const [user, setUser] = useState<UserProps | null>(null);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      if(user){
+        setUser({
+          uid: user.uid,
+          name: user?.displayName,
+          email: user?.email
+        })
+        setLoading(false);
+      }else{
+        setUser(null);
+        setLoading(false);
+      }
+    })
+    unsub()
+  }, [])
+
+  return(
+    <AuthContext.Provider 
+    value={{signed: !!user,
+      loading
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import {router} from "./App";
+import { router } from "./App";
 import { RouterProvider } from 'react-router';
+
+import AuthProvider  from './contexts/auth';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
